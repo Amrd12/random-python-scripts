722aa74281539d397b85863947f4c4e25d33cb9b
amrd12 <elnbawya@gmail.com>
2024-04-15 01:54:11 +0200

    add tic tac toe

--- router.py
+++ router.py
@@ -1,13 +1,12 @@
-from selenium import webdriver
-import time
-bro = webdriver.Edge()
-bro.get("http://192.168.1.1/")
-user = bro.find_elements("name", "Username")[0]
-pas = bro.find_elements("name", "Password")[0]
-sub = bro.find_element("id", "LoginId")
-
-
-user.send_keys("admin")
-pas.send_keys("amr12alnbawy")
-sub.click()
-time.sleep(30)
+import requests
+url = "http://192.168.1.1/"
+token = {
+    "action" :"login",
+    "Username" : "admin",
+    "Password" : "amr12alnbawy"
+}
+headers = { "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42"}
+s = requests.session()
+s.post("http://192.168.1.1/start.ghtml",data=token , headers=headers)
+req = s.get(url ="http://192.168.1.1/start.ghtml",headers = headers)
+print(req.text,req.status_code)
